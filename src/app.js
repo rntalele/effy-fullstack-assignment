90e79547ef1ae185d4d32a9b7515d728e72950f0
Rohan Talele <rohantalele@Rohans-MacBook-Pro.local>
2023-01-16 23:59:20 +0530

    Added Company List and User List Pages with CRUD Functionalities

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require("cors");
 
 const app = express();
 
 const routes = require("./routes/v1");
 
 app.use(express.json());
+
+app.use(cors());
+app.options("*", cors());
 
 app.use('/v1',routes);
 
